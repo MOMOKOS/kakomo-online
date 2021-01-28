7f6a36a0ad54659c23b729d358bab8290a3e8dbb
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-28 09:14:11 +0900

    商品出品機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,27 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @foods = Food.order('created_at DESC')
   end
-  
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :price, :description, :genre_id, :drink_id, :scene_id, :allergy_id, :day_id,
+                                 :image).merge(user_id: current_user.id)
+  end
 end
--- db/migrate/20210126121346_create_foods.rb
+++ db/migrate/20210126121346_create_foods.rb
@@ -1,7 +1,15 @@
 class CreateFoods < ActiveRecord::Migration[6.0]
   def change
     create_table :foods do |t|
-
+      t.string :name,             null: false
+      t.integer :price,           null: false
+      t.text :description,        null: false
+      t.integer :genre_id,        null: false
+      t.integer :drink_id,        null: false
+      t.integer :scene_id,        null: false
+      t.integer :allergy_id,      null: false
+      t.integer :day_id,          null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
